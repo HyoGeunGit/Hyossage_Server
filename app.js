6d7902d96738a02762da381d8db6fa26db183d7f
HyoGeunGit <shimhg02@naver.com>
2020-04-09 15:30:12 +0900

    test server

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ require('./routes/auth')(app,Users,rndstring);
 require('./routes/notice')(app, Message,rndstring);
 let port = 6101;
 
-app.listen(port, ()=>{
-  console.log('Server Porting On ' + port);
-})
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 
 module.exports = app;
--- mongo/index.js
+++ mongo/index.js
@@ -2,15 +2,17 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var db = mongoose.connect('mongodb://localhost/Me');
+MONGODB_URI='mongodb://shimhg02:adad123@ds133557.mlab.com:33557/heroku_wh8bw68p'
+mongoose.connect(MONGODB_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () { console.log("Mongo On"); });
 
+
 var UsersSchema = mongoose.Schema({
   id : {type : String},
-  name: {type: String},
+  name: {type : String},
   passwd : {type : String},
   token : {type: String}
 });
